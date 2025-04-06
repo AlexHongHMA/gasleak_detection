5ac69831a961529b96d7d0638892e8dab27c5fde
AlexHongHMA <htetmyataung.ctla@gmail.com>
2025-04-06 14:17:17 +0700

    Improve model architecture with L2 regularization and adjusted dropout rates.

--- src/model/model.py
+++ src/model/model.py
@@ -1,4 +1,5 @@
 from keras import layers, models
+import tensorflow as tf
 
 def cnn_3d_model(input_shape, num_classes=2):
     model = models.Sequential()
@@ -28,17 +29,19 @@ def cnn_3d_model(input_shape, num_classes=2):
     model.add(layers.Conv3D(32, (5, 5, 5), activation='relu', padding='same'))
     model.add(layers.LayerNormalization())
     model.add(layers.MaxPooling3D(pool_size=(3, 3, 3), padding='same'))
-    model.add(layers.Dropout(0.1))
+    model.add(layers.Dropout(0.15))
 
     # Flatten the output before feeding into fully connected layers
     model.add(layers.Flatten())
 
-    # Fully Connected Layer 1 with Dropout
-    model.add(layers.Dense(640, activation='relu'))
+    # Fully Connected Layer 1 with Dropout and L2 Regularization
+    model.add(layers.Dense(640, activation='relu', 
+                          kernel_regularizer=tf.keras.regularizers.l2(0.001)))
     model.add(layers.Dropout(0.3))  
 
-    # Fully Connected Layer 2 with Dropout
-    model.add(layers.Dense(128, activation='relu'))
+    # Fully Connected Layer 2 with Dropout and L2 Regularization
+    model.add(layers.Dense(128, activation='relu',
+                          kernel_regularizer=tf.keras.regularizers.l2(0.001)))
     model.add(layers.Dropout(0.3))  
 
     # Output Layer with 2 classes
